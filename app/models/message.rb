6591633477834f473dd91206169819f9fa602ca6
Andrei Gliga <otzy_007@yahoo.com>
2014-04-12 15:16:45 +0300

    user_id si conversation_id la message

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,7 @@
 class Message < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :conversation
+
   has_attached_file :file, {
       :url => "/system/:class/:id_partition/:style/:basename:hash:updated_at.:extension",
       :hash_secret => "93urfoekh390rfhkdjvbsdfoi0eghdfkh03tughrdslkgsh0-3uhgb4egfklsf@#bhsl!#(hlkbhe093h1Ahr093ehgblaq09384"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140412121103) do
+ActiveRecord::Schema.define(version: 20140412121554) do
 
   create_table "conversations", force: true do |t|
     t.datetime "created_at"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20140412121103) do
     t.string   "file_content_type"
     t.integer  "file_file_size"
     t.datetime "file_updated_at"
+    t.integer  "conversation_id"
+    t.integer  "user_id"
   end
 
   create_table "user_conversations", force: true do |t|
