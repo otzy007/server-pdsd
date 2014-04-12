bb82ed38244e3665ed82044d1de65c2a6352be0d
Andrei Gliga <otzy_007@yahoo.com>
2014-04-12 15:24:41 +0300

    belongs_to pt user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable
+
+  has_many :user_conversations
+  has_many :conversations, :through => :user_conversations
 end
