67167e09509b8af2919e527b2318ca8d0b93d3cf
Andrei Gliga <otzy_007@yahoo.com>
2014-04-14 22:20:09 +0300

    nume utilizator

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140414182216) do
+ActiveRecord::Schema.define(version: 20140414191313) do
 
   create_table "conversations", force: true do |t|
     t.datetime "created_at"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20140414182216) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["number"], name: "index_users_on_number", unique: true
